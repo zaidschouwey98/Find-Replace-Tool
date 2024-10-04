2eb1ff59e5c8af120b584f6715993094a802235a
Schouwey <zazar981@hotmail.com>
2024-10-04 11:50:03 +0200

    update maven plugin

--- src/main/java/ch/heigvd/dai/Main.java
+++ src/main/java/ch/heigvd/dai/Main.java
@@ -1,7 +1,30 @@
 package ch.heigvd.dai;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.concurrent.Callable;
+import picocli.CommandLine;
+
+@CommandLine.Command(
+        name = "hello",
+        description = "Print a 'Hello World!' type of message.",
+        version = "1.0.0",
+        mixinStandardHelpOptions = true)
+class Main implements Callable<Integer> {
+
+    @CommandLine.Parameters(
+            index = "0",
+            description = "The name of the user (default: World).",
+            defaultValue = "World")
+    protected String name;
+
+    @Override
+    public Integer call() {
+        System.out.println("Hello " + name + "!");
+        return 0;
+    }
+
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new Main()).execute(args);
+
+        System.exit(exitCode);
     }
 }
